976557fe0d718211e9e81074fbf351adc24a0dd5
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-02-20 21:50:22 +0100

    javascript excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+    if ((from < 0) || (to < 0)) return "ERROR";
+    if (typeof from !== "number"
+        || typeof to !== "number") return "ERROR";
 
+
+    let finalSum = 0;
+    const start = Math.min(from, to);
+    const end = Math.max(from, to);
+    for (let i = start; i <= end; i++) {
+        finalSum += i
+    }
+
+    return finalSum
 };
 
 // Do not edit below this line
