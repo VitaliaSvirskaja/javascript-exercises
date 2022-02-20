89dc754d14b56532a8abae6d6f7b2ef3a44a2c21
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-02-20 11:50:15 +0100

    javascript excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
+const reverseString = function(input) {
+    const splitInput = input.split('');
+    const reversedInput = splitInput.reverse();
+    return reversedInput.join('');
+}
 
 // Do not edit below this line
 module.exports = reverseString;
