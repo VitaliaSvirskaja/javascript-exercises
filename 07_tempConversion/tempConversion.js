2badc2eb9e3ca95ac915287b7e26626731df2d46
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-02-21 20:36:39 +0100

    javascript excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+    const result = (fahrenheit - 32) * (5 / 9);
+    return round(result);
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+    const result = (celsius * (9 / 5)) + 32;
+    return round(result);
 };
+
+function round(number) {
+    return Math.round(number * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
+    round
 };
