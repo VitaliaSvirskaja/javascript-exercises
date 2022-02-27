5aedd6cd2ab4198b5c326755b16be8f52123b76e
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-02-27 21:49:41 +0100

    javascript excercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	return firstNumber+secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber-secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(
+        (previousValue, currentValue) => previousValue +currentValue,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) return 0 ;
+return array.reduce(
+    (previousValue, currentValue)=> previousValue * currentValue)
 };
 
-const power = function() {
-	
+const power = function(...array) {
+  return array.reduce(
+      (previousValue, currentValue) => previousValue ** currentValue)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0)
+    return 1;
+  else
+    return (number * factorial(number - 1));
 };
 
 // Do not edit below this line
